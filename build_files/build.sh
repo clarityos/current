a83b6d1aca6b25cf2717fabb85f793a56e3f9c05
LinuxAbyss <zyorks.dev@gmail.com>
2025-09-28 14:47:30 -0700

    update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -7,11 +7,9 @@ set -euxo pipefail
 FEDORA_VERSION=$(rpm -E %fedora)
 CLARITY_VERSION="$FEDORA_VERSION"
 
-# Brave
-curl -fsSLo /etc/yum.repos.d/brave-browser.repo \
-    https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-
-# VSCodium
+# -------------------------------------------------------------
+# 1️⃣ Repositories (VSCodium)
+# -------------------------------------------------------------
 rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
 cat <<EOF >/etc/yum.repos.d/vscodium.repo
 [gitlab.com_paulcarroty_vscodium_repo]
@@ -25,25 +23,31 @@ metadata_expire=1h
 EOF
 
 # -------------------------------------------------------------
-# 4️⃣ Applications
+# 2️⃣ Applications (excluding Brave RPM)
 # -------------------------------------------------------------
-dnf5 -y install brave-browser codium kvantum materia-kde \
+dnf5 -y install codium kvantum materia-kde \
     papirus-icon-theme papirus-icon-theme-dark papirus-icon-theme-light
 dnf5 -y remove libreoffice\* kde-games\* kde-education\* plasma-welcome kate || true
 
-# Default browser
-update-alternatives --install /usr/bin/x-www-browser x-www-browser /usr/bin/brave-browser-stable 200
-update-alternatives --set x-www-browser /usr/bin/brave-browser-stable
-xdg-settings set default-web-browser brave-browser.desktop || true
-
 # -------------------------------------------------------------
-# 5️⃣ Flatpak
+# 3️⃣ Flatpak
 # -------------------------------------------------------------
+# Remove default Fedora remote
 flatpak remote-delete --if-exists fedora
+
+# Add Flathub remote
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
+# Install Brave via Flatpak
+flatpak install -y flathub com.brave.Browser
+flatpak override --user --filesystem=xdg-download com.brave.Browser
+
+# Set default browser (system-wide)
+update-alternatives --install /usr/bin/x-www-browser x-www-browser /usr/bin/flatpak 200
+update-alternatives --set x-www-browser /usr/bin/flatpak
+
 # -------------------------------------------------------------
-# 6️⃣ Branding
+# 4️⃣ Branding
 # -------------------------------------------------------------
 cat > /etc/os-release <<EOF
 NAME="ClarityOS"
@@ -57,6 +61,7 @@ HOME_URL="https://clarityos.org"
 SUPPORT_URL="https://clarityos.org/support"
 BUG_REPORT_URL="https://clarityos.org/issues"
 EOF
+
 ln -sf /etc/os-release /usr/lib/os-release
 dbus-uuidgen --ensure=/etc/machine-id
 
@@ -69,28 +74,25 @@ rpm-ostree compose tree \
     /ctx/treefile.json
 
 # -------------------------------------------------------------
-# 7️⃣ Graphics / Wallpaper
+# 5️⃣ Graphics / Wallpaper
 # -------------------------------------------------------------
 install -Dm644 /ctx/files/clarityos.png /usr/share/pixmaps/clarityos.png
 install -Dm644 /ctx/files/wallpaper.jpg /usr/share/wallpapers/clarityos/wallpaper.jpg
 
 # -------------------------------------------------------------
-# 8️⃣ User Skeleton
+# 6️⃣ User Skeleton
 # -------------------------------------------------------------
 rm -rf /etc/skel/*
 cp -r /ctx/skel/. /etc/skel/
 
-# -------------------------------------------------------------
-# 9️⃣ BGRT Boot Logo
-# -------------------------------------------------------------
-install -Dm644 /ctx/files/clarityos.bmp /usr/share/bootlogos/clarityos.bmp
 
 # -------------------------------------------------------------
-# 🔟 Plymouth Boot Watermark
+# 8️⃣ Plymouth Boot Watermark
 # -------------------------------------------------------------
 install -Dm644 /ctx/files/watermark.png /usr/share/plymouth/themes/spinner/watermark.png
 
 # -------------------------------------------------------------
-# 1️⃣1️⃣ Cleanup
+# 9️⃣ Cleanup
 # -------------------------------------------------------------
 dnf5 clean all
+flatpak repair --user
